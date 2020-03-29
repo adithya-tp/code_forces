bdcc59fe02ab28b9ffb65396802d2374d438deb8
adithya-tp <adithyatp@yahoo.com>
2020-03-29 22:21:02 +0400

    Technocup 2019 elimination round 1 - Problem B optimized

--- technocup_2019_er1/B/B.cpp
+++ technocup_2019_er1/B/B.cpp
@@ -20,50 +20,14 @@ then the grasshopper is within the field, else no.
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Point {
-    int x, y;
-    Point(int x, int y) {
-        this->x = x;
-        this->y = y;
-    }
-};
-
-double dist(Point a, Point b) {
-    return double(sqrt(pow(a.x - b.x, 2) + pow(a.y - b.y, 2)));
-}
-
-double area(Point a, Point b, Point c) {
-    double s = (dist(a, b) + dist(b, c) + dist(c, a)) / 2.0;
-    return sqrt(s * (s - dist(a, b)) * (s - dist(b, c)) * (s - dist(c, a)));
-}
-
-// Naive double equality is quite error prone.
-// This function makes up for that.
-bool double_equals(double a, double b, double epsilon = 0.001)
-{
-    return std::abs(a - b) < epsilon;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, d;
-    int t;
-    cin >> n >> d;
-    Point A(0, d);
-    Point B(d, 0);
-    Point C(n, n - d);
-    Point D(n - d, n);
-    double rect_area = dist(A, B) * dist(B, C);
-
-    cin >> t;
-    int x, y;
+    int n, d, t, x, y;
+    cin >> n >> d >> t;
     while(t--) {
         cin >> x >> y;
-        Point p = Point(x, y);
-        double sum_area = area(p, A, B) + area(p, B, C) + area(p, C, D) + area(p, D, A);
-        // Nan check coz doubles are muhfuckin crazyy in c pluh pluh
-        if(sum_area < rect_area || double_equals(sum_area, rect_area) || isnan(sum_area))
+        if((x + y >= d) && (x + y <= 2 * n - d) && (x - y >= -d) && (x - y <= d))
             cout << "YES\n";
         else
             cout << "NO\n";
