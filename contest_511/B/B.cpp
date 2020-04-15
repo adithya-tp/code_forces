e49efd8d58ad35339198d3e834f53131977949c4
adithya-tp <adithyatp@yahoo.com>
2020-04-15 22:53:15 +0400

    Contest 511 - Problem B - The simpler and optimal solution

--- contest_511/B/B.cpp
+++ contest_511/B/B.cpp
@@ -1,17 +1,21 @@
 /* Iteration 1:
  * Find the element with the max squared distance.
  * The final answer is just the sum of coordinates of this max point.
+ * 
+ * Iteration 2:
+ * My eagerness to solve the problem using "<complex>" made me overlook 
+ * the simpler solution. Iteration 1 would work with the simpler sum of 
+ * coordinates rather than the squared distance (since we are just dealing 
+ * with the first quadrant). The answer is simply the max sum seen so far.
+ * 
+ * For both approaches the reason why we simply sum the coordinates is because 
+ * we are trying to find 'c' in (a / c) + (b / c) = 1. Solving for 'c', this
+ * is just a + b, ie, the sum of the coordinates. Obviously, we want to find the c
+ * for the point that's furthest away from the origin.
  */
 
 #include <bits/stdc++.h>
 using namespace std;
-
-typedef complex<int> pt;
-#define x real()
-#define y imag()
-long long int distance_without_root(pt p) {
-	return p.x * p.x + p.y * p.y;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -19,16 +23,10 @@ int main() {
     int n, a, b;
 	cin >> n;
 	long long max_d = INT_MIN;
-	pt max_pt{0,0};
 	while(n--) {
 		cin >> a >> b;
-		pt p{a, b};
-		long long int d = distance_without_root(p);
-		if(d > max_d) {
-			max_pt = p;
-			max_d = d;
-		}		
+		max_d = max_d > a + b ? max_d : a + b;
     }
-    cout << max_pt.x + max_pt.y << "\n";
+    cout << max_d << "\n";
     return 0;
 }
