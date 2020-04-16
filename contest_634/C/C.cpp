4cfd17621ed350a27eb5ad8c454a8cc5af83af7f
adithya-tp <adithyatp@yahoo.com>
2020-04-16 12:28:12 +0400

    Contest 634 - Problem C updated using shoobs explanation

--- contest_634/C/C.cpp
+++ contest_634/C/C.cpp
@@ -10,29 +10,44 @@
  * Came across a solution similar to mine. However, they only used a single map.
  * But still kept track of the max value in the map though. If the total_size of map
  * was equal to max value, then answer = total_size - 1, else minimum between 
- * total_size and max value is the answer. Still need to convice myself why this works.
+ * total_size and max value is the answer.
+ * 
+ * I understand how this works now.
+ * 
+ * You keep track of the counts of all elements in a map. The number of unique 
+ * elements (count.size()). Now we iterate through all the elements in the map. 
+ * If count.size() > count[element], then ans = max(count[element], ans). 
+ * If count[element] < count.size(), then ans = max(count.size(), ans).
+ * In each of the above cases, we choose the min of the two quantities considered in the
+ * equality, since we are constrained to have equal number of elements in a team.
+ * Now if count[element] == count.size(), then ans = count.size() - 1 = count[element] - 1.
+ * (best understood by trying out an example like {1, 2, 3, 3, 3})
  */ 
 #include <bits/stdc++.h>
 using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t, num, n, ans, max_multiple = INT_MIN;
+    int t, num, ans, n;
     cin >> t;
     while(t--) {
 		unordered_map<int, int> count;
-		max_multiple = INT_MIN;
 		cin >> num;
         for(int i = 0; i < num; i++) {
 			cin >> n;
 			count[n]++;
-			max_multiple = max(max_multiple, count[n]);
 		}
-		ans = count.size();
-		if(ans == max_multiple)
-			cout << ans - 1 << "\n";
-		else
-			cout << min(ans, max_multiple) << "\n";
+		ans = INT_MIN;
+		auto it = count.begin();
+		int l = count.size();
+		while(it != count.end()) {
+			if(l == it->second)
+				ans = max(ans, l - 1);
+			else
+				ans = max(ans, min(l, it->second));
+			it++;
+		}
+		cout << ans << "\n";
     }
     return 0;
 }
