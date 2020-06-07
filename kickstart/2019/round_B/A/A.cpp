e8720b684ecd39a971ddcb31eef69a8edc36a881
adithya-tp <adithyatp@yahoo.com>
2020-06-07 13:42:29 +0400

    Google Kickstart 2019 Round B - Problem A (small input, TLE on large)

---
+++ kickstart/2019/round_B/A/A.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for(int i = 0; i < (int)(n); i++)
+#define for1(i, n) for(int i = 1; i < (int)(n); i++)
+#define ford(i, n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define fore(i, a, t) for(int i = (int)(a); i <= (int)(b); i++)
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long i64;
+typedef vector<i64> vi64;
+typedef vector<vi64> vvi64;
+typedef pair<i64, i64> pi64;
+typedef double ld;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.precision(10);
+	cout << fixed;
+	int t, n, q, l, r, odd_counts;
+	cin >> t;
+	forn(i, t) {
+		int ans = 0;
+		cin >> n >> q;
+		vector<char> str(n);
+		forn(j, n)
+			cin >> str[j];
+		forn(j, q) {
+			odd_counts = 0;
+			unordered_map<char, int> counts;
+			cin >> l >> r;
+			if(l == r) {
+				ans++;
+				continue;
+			}
+			for(int k = l - 1; k < r; k++) {
+				counts[str[k]]++;
+				if(counts[str[k]] % 2 == 1)
+					odd_counts++;
+				else
+					odd_counts--;
+			}
+			if((l - r + 1) % 2 == 0) {
+				if(odd_counts == 0) {
+					ans++;
+				}
+			}
+			else {
+				if(odd_counts == 1) {
+					ans++;
+				}
+			}
+		}
+		cout << "Case #" << i + 1 << ": " << ans << "\n";
+	}
+	return 0;
+}
