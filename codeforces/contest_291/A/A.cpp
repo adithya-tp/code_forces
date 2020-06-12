187680e07ca8f8fdf48af966e0d456f52e9ee677
adithya-tp <adithyatp@yahoo.com>
2020-06-12 11:21:33 +0400

    Contest 291 - Problem A

---
+++ codeforces/contest_291/A/A.cpp
@@ -0,0 +1,55 @@
+/* Iteration 1:
+ * You only really need to invert numbers > 4 with the exception of
+ * leading 9 since this would result in a leading 0, which is not
+ * permitted in the problem.
+ */
+#include <bits/stdc++.h>
+
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define forn(i, n) for(int i = 0; i < (int)(n); i++)
+#define for1(i, n) for(int i = 1; i < (int)(n); i++)
+#define ford(i, n) for(int i = (int)(n) - 1; i >= 0; i--)
+#define fore(i, a, t) for(int i = (int)(a); i <= (int)(b); i++)
+
+using namespace std;
+
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vpi;
+typedef vector<vi> vvi;
+typedef long long i64;
+typedef vector<i64> vi64;
+typedef vector<vi64> vvi64;
+typedef pair<i64, i64> pi64;
+typedef double ld;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+	cout.precision(10);
+	cout << fixed;
+	string s, ans="";
+	cin >> s;
+	int i = 0;
+	if(s[0] == '9') {
+		ans += s[i];
+		i++;
+	}
+	char c;
+	for(int j = i; j < (int)s.length(); j++) {
+		int dig = s[j] - '0';
+		if(dig > 4)
+			c = (9 - dig) + '0';
+		else
+			c = dig + '0';
+		ans += c;
+	}
+	cout << ans << "\n";
+	return 0;
+}
